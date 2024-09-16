9ba1bfaf39ea99579700db0195b58e63e0be0716
EnmmmmOvO <wangjinghan0218@gmail.com>
2024-09-17 01:26:28 +1000

    update readme format

--- readme.py
+++ readme.py
@@ -1,14 +1,12 @@
 import datetime
 import sys
 
-
-def generate_schedule(days):
-    current_date = datetime.datetime.now()
-
-    start_of_week = current_date - datetime.timedelta(days=current_date.weekday())
+def generate_schedule(days, start_date):
+    start_of_week = start_date - datetime.timedelta(days=start_date.weekday())
 
     schedule = []
-
+    
+    t = 1
     for week in range(1, 11):  # We need 11 weeks to skip the 6th one
         if week == 6:  # Skip the 6th week
             continue
@@ -20,10 +18,11 @@ def generate_schedule(days):
             day_date = start_of_week + datetime.timedelta(days=(week - 1) * 7 + day)
             formatted_date = day_date.strftime("%Y-%m-%d")
             if num == 0:
-                week_schedule += f"- Recording [{day:02d} ({formatted_date})]()  \n"
+                week_schedule += f"- Recording [{t:02d} ({formatted_date})]()  \n"
                 num = 1
             else:
-                week_schedule += f"&emsp;&emsp;&emsp;&emsp;&emsp;[02 ({formatted_date})]()  \n"
+                week_schedule += f"&emsp;&emsp;&emsp;&emsp;&emsp;[{t:02d} ({formatted_date})]()  \n"
+            t += 1
 
         week_schedule += "- Slide [](./slide)  \n"
         week_schedule += "&emsp;&emsp;&ensp;[](./slide)  \n"
@@ -32,16 +31,20 @@ def generate_schedule(days):
 
     return "\n".join(schedule)
 
-
 if __name__ == '__main__':
-    if len(sys.argv) != 4 and len(sys.argv) != 5:
-        print("Usage: python readme.py <Course Name> <Course Number> <Day of the week (1-7)> <Day of the week (1-7)>")
+    if len(sys.argv) < 4:
+        print("Usage: python readme.py <Course Name> <Course Number> <Start Date (yyyymmdd)> <Day of the week (1-7)> <Day of the week (1-7)>")
         sys.exit(1)
 
-    days_of_week = [int(sys.argv[3]) - 1]
+    try:
+        start_date = datetime.datetime.strptime(sys.argv[3], "%Y%m%d").date()
+    except ValueError:
+        print("Invalid date format. Please use yyyymmdd format.")
+        sys.exit(1)
 
-    if len(sys.argv) == 5:
-        days_of_week.append(int(sys.argv[4]) - 1)
+    days_of_week = []
+    for i in range(4, len(sys.argv)):
+        days_of_week.append(int(sys.argv[i]) - 1)
 
     with open('README.md', 'w') as f:
-        f.write(f"# {sys.argv[1]} {sys.argv[2]}\n\n" + generate_schedule(days_of_week))
+        f.write(f"# {sys.argv[1]} {sys.argv[2]}\n\n" + generate_schedule(days_of_week, start_date))
